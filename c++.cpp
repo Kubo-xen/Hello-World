17f6f4ac8683cacdab48470e9eed846c27c177b0
Kubo-xen <abriansahd@gmail.com>
2026-02-07 21:52:21 +0700

    memasukkan file pertama

---
+++ c++.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string HW = "Hello World with var";
+
+    cout << HW << endl;
+    cout << "Hello World with without var" << endl;
+
+    return 0;
+}
